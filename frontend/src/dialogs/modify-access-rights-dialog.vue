341acf7bcd68bbff1a44b0ef1a74a32ee39fbb8e
Cyrill Püntener <wp99cp@gmx.net>
2025-11-18 13:09:37 +0100

    chore: clean up permissions dialog (prepare for fixing #933)

--- frontend/src/dialogs/modify-access-rights-dialog.vue
+++ frontend/src/dialogs/modify-access-rights-dialog.vue
@@ -3,7 +3,7 @@
         <template #title>Change Access Rights</template>
 
         <template #content>
-            <configure-access-rights
+            <access-rights-manager
                 v-model="modifiableAccessRights"
                 :min-access-rights="minAccessRights"
             />
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import ConfigureAccessRights from 'components/configure-access-rights/configure-access-rights.vue';
+import AccessRightsManager from 'components/configure-access-rights/access-rights-manager.vue';
 import { useDialogPluginComponent } from 'quasar';
 import BaseDialog from 'src/dialogs/base-dialog.vue';
 import { useUpdateAccessRightsMutation } from 'src/hooks/mutation-hooks';
@@ -30,7 +30,6 @@ import {
     useProjectAccessRights,
 } from 'src/hooks/query-hooks';
 import { useEditablePaginatedResponse } from 'src/hooks/utility-hooks';
-import { unref, watch } from 'vue';
 
 const { projectUuid: projectUuid } = defineProps<{ projectUuid: string }>();
 
@@ -39,14 +38,6 @@ const { dialogRef, onDialogOK } = useDialogPluginComponent();
 const minAccessRights = useMinimalAccessRightsForNewProject();
 const { data: projectAccess } = useProjectAccessRights(projectUuid);
 const modifiableAccessRights = useEditablePaginatedResponse(projectAccess);
-
-// debug watch
-watch(modifiableAccessRights, () => {
-    console.debug(
-        `project access rights for project ${projectUuid} got modified to:
-${JSON.stringify(unref(modifiableAccessRights), null, 2)}`,
-    );
-});
 
 const { mutate: changeAccessRights } = useUpdateAccessRightsMutation(
     projectUuid,
